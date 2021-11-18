5044e34e700501a15cc5fd614da7f3abcf2007e8
srujana-edu <93914567+srujana-edu@users.noreply.github.com>
2021-11-18 12:55:42 +0100

    'helloworld'

---
+++ script.js
@@ -0,0 +1,4 @@
+document.write('<center><h1><font color="navy">srujana</font></h1></center>');
+document.write('<p><font color="green">Jag har masterexamen inom systemvetenskap (MCA).</br>Jag lärde mig programmeringspråk1 och programmeringspråk2 i tyresö gymansium</font></p>');
+document.write('<left><font color="brown"><ul><li>Jag har gjort många projekt när jag utbildade mig</li><li>Jag är hjälpsam och jag är väldigt  positiv.</li><li>Jag är mycket intresserad av att samarbeta. Jag är intresserad av lär mig nya saker.</li></ul></font></left>');
+console.log("Hello World");
